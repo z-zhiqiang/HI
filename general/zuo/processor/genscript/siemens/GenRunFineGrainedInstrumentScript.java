fa0413d112067f45835d4e219dfcedc7a20befca
sunzzq <sunzzq@localhost>
2013-04-25 22:52:48 +0800

    update

---
+++ general/zuo/processor/genscript/siemens/GenRunFineGrainedInstrumentScript.java
@@ -0,0 +1,96 @@
+package zuo.processor.genscript.siemens;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import zuo.util.file.FileUtility;
+
+
+public class GenRunFineGrainedInstrumentScript extends AbstractGenRunScript implements GenRunInstrumentScript {
+	final String traceDir;
+	private final List<Integer> failingTests;
+	private final List<Integer> passingTests;
+	
+	
+	public GenRunFineGrainedInstrumentScript(String sub, String ver, String cc, String sD, String eD, String oD, String scD, String tD, String failing, String passing) {
+		super(sub, ver, cc, sD, eD, oD, scD);
+		this.traceDir = tD;
+		this.mkOutDir();
+		this.failingTests = FileUtility.readInputsArray(failing);
+		this.passingTests = FileUtility.readInputsArray(passing);
+	}
+
+
+	@Override
+	public void genRunScript() {
+		StringBuffer code = new StringBuffer();
+		code.append(compileCommand + "\n");
+		code.append("echo script: " + version + "\n");
+		code.append("export VERSIONSDIR=" + executeDir + "\n");
+		code.append("export OUTPUTSDIR=" + outputDir + "\n");
+		code.append(startTimeCommand + "\n");
+		
+		for (Iterator it = failingTests.iterator(); it.hasNext();) {
+			int index = (Integer) it.next();
+			code.append(runinfo + index + "\"\n");// running info
+			code.append("export SAMPLER_FILE=" + traceDir + "o" + index + ".fprofile\n");
+			code.append("$VERSIONSDIR/" + version + "_finst.exe ");//executables
+			code.append(inputsMap.get(index));//parameters
+			code.append(" >& $OUTPUTSDIR/o" + index + ".fout\n");//output file
+		}
+		
+		for (Iterator it = passingTests.iterator(); it.hasNext();) {
+			int index = (Integer) it.next();
+			code.append(runinfo + index + "\"\n");// running info
+			code.append("export SAMPLER_FILE=" + traceDir + "o" + index + ".pprofile\n");
+			code.append("$VERSIONSDIR/" + version + "_finst.exe ");//executables
+			code.append(inputsMap.get(index));//parameters
+			code.append(" >& $OUTPUTSDIR/o" + index + ".pout\n");//output file
+		}
+		
+		code.append(endTimeCommand + " >& " + outputDir + "time");
+		printToFile(code.toString(), scriptDir, version + "_fg.sh");
+	}
+
+
+	@Override
+	protected void mkOutDir() {
+		//make directory for outputs
+		File fo = new File(outputDir);
+		if(!fo.exists()){
+			fo.mkdirs();
+		}
+		
+		//make directory for traces
+		File ft = new File(traceDir);
+		if(!ft.exists()){
+			ft.mkdirs();
+		}
+	}
+	
+	
+//	public void genInstrumentPredicateScripts(){
+//		final String instrumentCommand = "sampler-cc $SCHEME -fno-sample "; 
+//		final String extractSiteCommand = "$EXTRACTDIR/extract-section .debug_site_info ";
+//		
+//		StringBuffer code = new StringBuffer();
+//		code.append("echo instrumenting script: " + subject + "\n");
+//		code.append("export ROOTDIR=" + rootDir + subject + "\n");
+//		code.append("export EXTRACTDIR=" + extractToolsDir + "\n");
+//		code.append("export SCHEME=\"" + finerGrainedScheme + "\"\n");
+//		
+//		String[] fs = new File(rootDir + subject, "/versions").list(new VersionFoldernameFilter());
+//		Arrays.sort(fs, new FoldernameComparator());
+//		for (int i = 0; i < fs.length; i++) {
+//			code.append(instrumentInfo + fs[i] + "\"\n");
+//			String dir = "$ROOTDIR/versions/" + fs[i] + "/";
+//			code.append(instrumentCommand + dir + subject + ".c -o " + dir + fs[i] + "_inst.exe\n");
+//			code.append(extractSiteCommand + dir + fs[i] + "_inst.exe > " + dir + "sites.txt\n");
+//		}
+//		code.append("echo instrumentation finished\n");
+//		
+//		printToFile(code.toString(), rootDir + subject + "/scripts", "fineGrainedInstrument.sh");
+//	}
+
+}
