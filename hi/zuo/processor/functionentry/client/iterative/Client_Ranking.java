b052fefeea0d9b448b5d04d3a938f256bab04a20
sunzzq <sunzzq@localhost>
2014-04-23 22:51:21 +0800

    stable version: adding new feature to get importance information within each function (for correlation and convergence)

--- hi/zuo/processor/functionentry/client/iterative/Client_Ranking.java
+++ hi/zuo/processor/functionentry/client/iterative/Client_Ranking.java
@@ -43,6 +43,7 @@ public class Client_Ranking {
 	
 	private final Map<String, Map<String, List<Object>>> correlationDataMap;
 	private final Map<String, List<Object>> correlationResultsMap;
+	private final Map<String, List<Object>> convergenceResultsMap;
 	
 	
 	public Client_Ranking(File rootDir, String subject, File consoleFolder, int startV, int endV) {
@@ -55,6 +56,7 @@ public class Client_Ranking {
 		
 		this.correlationDataMap = new LinkedHashMap<String, Map<String, List<Object>>>();
 		this.correlationResultsMap = new LinkedHashMap<String, List<Object>>();
+		this.convergenceResultsMap = new LinkedHashMap<String, List<Object>>();
 	}
 
 	
@@ -88,7 +90,7 @@ public class Client_Ranking {
 			FunctionEntrySites cSites = new FunctionEntrySites(new File(version, vi + "_c.sites"));
 			FunctionEntryProfile[] cProfiles = new FunctionEntryProfileReader(new File(rootDir, subject + "/traces/" + vi + "/coarse-grained"), cSites).readFunctionEntryProfiles();
 			
-			runCorrelationData(vi, sInfo, fProfiles, cProfiles);
+			runForRankingInfo(vi, sInfo, fProfiles, cProfiles);
 		}
 		
 		printCorrelationToExcel();
@@ -139,7 +141,7 @@ public class Client_Ranking {
 				FunctionEntrySites cSites = new FunctionEntrySites(new File(subversion, vi + "_c.sites"));
 				FunctionEntryProfile[] cProfiles = new FunctionEntryProfileReader(new File(rootDir, subject + "/traces/" + version.getName() + "/" + subversion.getName() + "/coarse-grained"), cSites).readFunctionEntryProfiles();
 				
-				runCorrelationData(vi, sInfo, fProfiles, cProfiles);
+				runForRankingInfo(vi, sInfo, fProfiles, cProfiles);
 			}
 		}
 		
@@ -147,7 +149,7 @@ public class Client_Ranking {
 	}
 
 	
-	private void runCorrelationData(String vi, SitesInfo sInfo,
+	private void runForRankingInfo(String vi, SitesInfo sInfo,
 			PredicateProfile[] fProfiles, FunctionEntryProfile[] cProfiles) {
 		//divide profiles
 		int passing = 0, failing = 0;
@@ -166,6 +168,7 @@ public class Client_Ranking {
 		Processor fgProcessor = new Processor(fProfiles, failing, passing);
 		fgProcessor.process();
 		ProcessorPreImportanceInfoWithinFun imProcessor = new ProcessorPreImportanceInfoWithinFun(fgProcessor.getPredictorsList());
+		imProcessor.process();
 		Map<String, PredicateImportanceInfoWithinFunction> ImportanceInfo = imProcessor.getImportanceInfoMap();
 		
 		//coarse-grained analysis
@@ -176,16 +179,24 @@ public class Client_Ranking {
 		filterFrequencyMap(cgProcessor.getFrequencyMap(), sInfo);
 		assert(cgProcessor.getFrequencyMap().size() == sInfo.getMap().size());
 		List<Entry<FunctionEntrySite, FrequencyValue>> list = sortFunctionEntrySiteMap(cgProcessor.getFrequencyMap(), sInfo);
+		assert(ImportanceInfo.size() == list.size());
 		
 		Map<String, List<Object>> correlationData = new LinkedHashMap<String, List<Object>>();
 		List<Object> correlationResults = new ArrayList<Object>();
-		processDSCorrelation(ImportanceInfo, list, correlationData, correlationResults);
+		List<Object> convergenceResults = new ArrayList<Object>();
+		processImportanceCorrelationConvergence(ImportanceInfo, list, correlationData, correlationResults, convergenceResults);
 
 		this.correlationDataMap.put(vi, correlationData);
 		this.correlationResultsMap.put(vi, correlationResults);
+		this.convergenceResultsMap.put(vi, convergenceResults);
 	}
 	
-	private List<Entry<FunctionEntrySite, FrequencyValue>> sortFunctionEntrySiteMap(
+	/**sort functions according to H_2 and LESS_FIRST
+	 * @param frequencyMap
+	 * @param sInfo
+	 * @return
+	 */
+	public static List<Entry<FunctionEntrySite, FrequencyValue>> sortFunctionEntrySiteMap(
 			Map<FunctionEntrySite, FrequencyValue> frequencyMap, final SitesInfo sInfo) {
 		// TODO Auto-generated method stub
 		List<Entry<FunctionEntrySite, FrequencyValue>> list = new ArrayList<Entry<FunctionEntrySite, FrequencyValue>>(frequencyMap.entrySet());
@@ -224,36 +235,88 @@ public class Client_Ranking {
 		}
 	}
 	
-	private void processDSCorrelation(Map<String, PredicateImportanceInfoWithinFunction> ImportanceInfo, List<Map.Entry<FunctionEntrySite, FrequencyValue>> list,
-			Map<String, List<Object>> correlationData, List<Object> correlationResults) {
-		//construct the DS map
-		for(int i = 0; i < list.size(); i++){
-			Map.Entry<FunctionEntrySite, FrequencyValue> entry = list.get(i);
-			String function = entry.getKey().getFunctionName();
-			List<Object> array = new ArrayList<Object>();
-			FrequencyValue value = entry.getValue();
-			array.add((double) (list.size() - i - 1));
-			array.add(value.getH_2());
-			array.add((double) value.getNegative());
-			array.add((double) value.getPositive());
-			if(ImportanceInfo.containsKey(function)){
-				PredicateImportanceInfoWithinFunction dsInfo = ImportanceInfo.get(function);
-				array.add(dsInfo.getMax_Importance());
-				array.add(dsInfo.getMean_Importance());
-				array.add(dsInfo.getMedian_Importance());
-			}
-			else{
-				array.add(0.0D);
-				array.add(0.0D);
-				array.add(0.0D);
-			}
-			if(correlationData.containsKey(function)){
-				throw new RuntimeException("multiple functions error");
-			}
-			correlationData.put(function, array);
-		}
+	private static void processImportanceCorrelationConvergence(Map<String, PredicateImportanceInfoWithinFunction> ImportanceInfo, List<Map.Entry<FunctionEntrySite, FrequencyValue>> list,
+			Map<String, List<Object>> correlationData, List<Object> correlationResults, List<Object> convergenceResults) {
+		
+		//construct the Importance map
+		constructData(ImportanceInfo, list, correlationData);
 		
 		//compute the Correlation Coefficient
+		computeCorrelationResults(correlationData, correlationResults);
+		
+		//compute the convergence information of importance
+		computeConvergenceResults(correlationData, convergenceResults);
+	}
+
+
+	private static void computeConvergenceResults(Map<String, List<Object>> correlationData, List<Object> convergenceResults) {
+		// TODO Auto-generated method stub
+		double max_importance = 0;
+		
+		int iterations_increase_full = 0;
+		int iterations_increase_partial = 0;
+		
+		double sum_full = 0.0D;
+		double sum_partial = 0.0D;
+		
+		int index_full = 0;
+		int index_partial = 0;
+
+		for(String function: correlationData.keySet()){
+			sum_full += max_importance;
+			index_full++;
+			if(!trivialCase(correlationData, function)){
+				sum_partial += max_importance;
+				index_partial++;
+			}
+			
+			double importance = (Double) correlationData.get(function).get(4);
+			if(importance > max_importance){
+				max_importance = importance;
+				
+				iterations_increase_full++;
+				if(!trivialCase(correlationData, function)){
+					iterations_increase_partial++;
+				}
+			}
+		}
+		
+		assert(index_full == correlationData.size());
+		assert(index_partial == getPartialSize(correlationData));
+		
+		
+		convergenceResults.add(max_importance);
+		
+		convergenceResults.add(iterations_increase_full);
+		convergenceResults.add(index_full);
+		convergenceResults.add(((double) iterations_increase_full) / index_full);
+		convergenceResults.add(sum_full);
+		convergenceResults.add(index_full * max_importance);
+		convergenceResults.add(sum_full / (index_full * max_importance));
+		
+		convergenceResults.add(iterations_increase_partial);
+		convergenceResults.add(index_partial);
+		convergenceResults.add(((double) iterations_increase_partial) / index_partial);
+		convergenceResults.add(sum_partial);
+		convergenceResults.add(index_partial * max_importance);
+		convergenceResults.add(sum_partial / (index_partial * max_importance));
+		
+	}
+
+
+	private static boolean trivialCase(Map<String, List<Object>> correlationData, String function) {
+		double h = (Double) correlationData.get(function).get(1);
+		return Math.abs(h - 0) < 0.000001;
+	}
+
+
+	/**compute the correlation coefficient between different values
+	 * @param correlationData
+	 * @param correlationResults
+	 */
+	private static void computeCorrelationResults(
+			Map<String, List<Object>> correlationData,
+			List<Object> correlationResults) {
 		double cc;
 		
 		int fullSize = correlationData.size();
@@ -310,7 +373,40 @@ public class Client_Ranking {
 		cc = computeCorrelationCoefficient(correlationData, 1, 6, false);
 		correlationResults.add(cc);
 		correlationResults.add(computeTValueOfCC(cc, partialSize));
-		
+	}
+
+
+	/**construct importance information within each function with H value
+	 * @param ImportanceInfo
+	 * @param list
+	 * @param correlationData: 0:index; 1:H; 2:neg; 3:pos; 4:max_importance; 5:mean_importance; 6:median_importance 
+	 */
+	private static void constructData(
+			Map<String, PredicateImportanceInfoWithinFunction> ImportanceInfo,
+			List<Map.Entry<FunctionEntrySite, FrequencyValue>> list,
+			Map<String, List<Object>> correlationData) {
+		for(int i = 0; i < list.size(); i++){
+			Map.Entry<FunctionEntrySite, FrequencyValue> entry = list.get(i);
+			String function = entry.getKey().getFunctionName();
+			List<Object> array = new ArrayList<Object>();
+			FrequencyValue value = entry.getValue();
+			
+			array.add((double) (list.size() - i - 1));
+			array.add(value.getH_2());
+			array.add((double) value.getNegative());
+			array.add((double) value.getPositive());
+			
+			assert(ImportanceInfo.containsKey(function));
+			PredicateImportanceInfoWithinFunction importanceInfo = ImportanceInfo.get(function);
+			array.add(importanceInfo.getMax_Importance());
+			array.add(importanceInfo.getMean_Importance());
+			array.add(importanceInfo.getMedian_Importance());
+			
+ 			if(correlationData.containsKey(function)){
+				throw new RuntimeException("multiple functions error");
+			}
+			correlationData.put(function, array);
+		}
 	}
 
 	public static double computeTValueOfCC(double cc, int size) {
@@ -318,13 +414,12 @@ public class Client_Ranking {
 		return cc / Math.sqrt((1 - cc * cc) / (size - 2));
 	}
 
-	private int getPartialSize(Map<String, List<Object>> correlationData) {
+	private static int getPartialSize(Map<String, List<Object>> correlationData) {
 		// TODO Auto-generated method stub
 		int size = 0;
 		for(String function: correlationData.keySet()){
-			List<Object> list = correlationData.get(function);
-			if(Math.abs((Double) list.get(2) - 0) < 0.0000001){
-				break;
+			if(trivialCase(correlationData, function)){
+				continue;
 			}
 			size++;
 		}
@@ -334,12 +429,11 @@ public class Client_Ranking {
 
 	/**
 	 * @param correlationData
-	 * 0:ordering index; 1:ds; 2:neg; 3:pos; 4:max_ds; 5:mean_ds; 6:median_ds;
 	 * @param i: variable
 	 * @param j: variable
 	 * @return
 	 */
-	private double computeCorrelationCoefficient(Map<String, List<Object>> correlationData, int i, int j, boolean fullList) {
+	public static double computeCorrelationCoefficient(Map<String, List<Object>> correlationData, int i, int j, boolean fullList) {
 		// TODO Auto-generated method stub
 		double sumi = 0, sumj = 0, sumii = 0, sumjj = 0, sumij = 0;
 		int size = 0;
@@ -377,10 +471,12 @@ public class Client_Ranking {
 		return ij / Math.sqrt(ii * jj);
 	}
 	
+	
 	private void printCorrelationToExcel(){
 		XSSFWorkbook workbook = new XSSFWorkbook();
+		printConvergenceResultsToExcel(workbook);
 		printCorrelationResultsToExcel(workbook);
-		printCorrelationDataToExcel(workbook);
+		printDataToExcel(workbook);
 		try {
 			if(!consoleFolder.exists()){
 				consoleFolder.mkdirs();
@@ -394,9 +490,72 @@ public class Client_Ranking {
 		}
 	}
 	
+	private void printConvergenceResultsToExcel(XSSFWorkbook workbook) {
+		// TODO Auto-generated method stub
+		XSSFSheet sheet = workbook.createSheet("Results_Convergence");
+		addConvergenceResultsTitle(sheet);
+		
+		int rownum = sheet.getPhysicalNumberOfRows();
+		for(String version: this.convergenceResultsMap.keySet()){
+			Row row = sheet.createRow(rownum++);
+			int cellnum = 0;
+			
+			Cell cell = row.createCell(cellnum++);
+			cell.setCellValue(version);
+			
+			for(Object object: this.convergenceResultsMap.get(version)){
+				cell = row.createCell(cellnum++);
+				if(object instanceof Integer){
+					cell.setCellValue((Integer) object);
+				}
+				else if(object instanceof Double){
+					cell.setCellValue((Double) object);
+				}
+				else if(object instanceof Long){
+					cell.setCellValue((Long) object);
+				}
+				else if(object instanceof String){
+					cell.setCellValue((String) object);
+				}
+			}
+		}
+	}
+
+	private void addConvergenceResultsTitle(XSSFSheet sheet) {
+		// TODO Auto-generated method stub
+		int rownum = sheet.getPhysicalNumberOfRows();
+		
+		Row row0 = sheet.createRow(rownum++);
+		int cellnum0 = 0;
+		Row row1 = sheet.createRow(rownum++);
+		int cellnum1 = 0;
+		
+		String[] ftitles = {"Full", "Partial"};
+		String[] titles = {"Iteration_increase", "Iteration", "Percent_iteration", "Sum", "Sum_total", "Percent_sum"};
+		
+		Cell cell0 = row0.createCell(cellnum0++);
+		cell0.setCellValue(" ");
+		Cell cell1 = row1.createCell(cellnum1++);
+		cell1.setCellValue(" ");
+		
+		cell0 = row0.createCell(cellnum0++);
+		cell0.setCellValue(" ");
+		cell1 = row1.createCell(cellnum1++);
+		cell1.setCellValue("Max_importance");
+		
+		for(int j = 0; j < ftitles.length; j++){
+			for(int i = 0; i < titles.length; i++){
+				cell0 = row0.createCell(cellnum0++);
+				cell0.setCellValue(ftitles[j]);
+				cell1 = row1.createCell(cellnum1++);
+				cell1.setCellValue(titles[i]);
+			}
+		}
+	}
+	
 	private void printCorrelationResultsToExcel(XSSFWorkbook workbook) {
 		// TODO Auto-generated method stub
-		XSSFSheet sheet = workbook.createSheet("Results");
+		XSSFSheet sheet = workbook.createSheet("Results_Correlation");
 		addCorrelationResultsTitle(sheet);
 		
 		int rownum = sheet.getPhysicalNumberOfRows();
@@ -435,8 +594,8 @@ public class Client_Ranking {
 		int cellnum1 = 0;
 		
 		String[] ftitles = {"Full", "Partial"};
-		String[] titles = {"Size", "Index-Max", "T", "DS-Max", "T", "Index-Mean", "T",
-				"DS-Mean", "T", "Index-Median", "T", "DS-Median", "T"};
+		String[] titles = {"Size", "Index-Max", "T", "H-Max", "T", "Index-Mean", "T",
+				"H-Mean", "T", "Index-Median", "T", "H-Median", "T"};
 		
 		Cell cell0 = row0.createCell(cellnum0++);
 		cell0.setCellValue(" ");
@@ -453,7 +612,7 @@ public class Client_Ranking {
 		}
 	}
 
-	private void printCorrelationDataToExcel(XSSFWorkbook workbook) {
+	private void printDataToExcel(XSSFWorkbook workbook) {
 		// TODO Auto-generated method stub
 		for(String version: this.correlationDataMap.keySet()){
 			XSSFSheet sheet = workbook.createSheet(version.replace('/', '_'));
@@ -494,7 +653,7 @@ public class Client_Ranking {
 		Row row0 = sheet.createRow(rownum++);
 		int cellnum0 = 0;
 		
-		String[] titles = {" ", "Index", "DS", "Negative", "Positive","Max_DS", "Mean_DS", "Median_DS"};
+		String[] titles = {" ", "Index", "H", "Negative", "Positive","Max_Importance", "Mean_Importance", "Median_Importance"};
 		
 		for(int i = 0; i < titles.length; i++){
 			Cell cell0 = row0.createCell(cellnum0++);
@@ -505,31 +664,31 @@ public class Client_Ranking {
 
 	public static void main(String[] args) {
 		String[][] argvs = {
-				{"809", "grep"},
-				{"213", "gzip"},
-				{"363", "sed"},
-				{"13585", "space"},
-				{"4130", "printtokens"},
-				{"4115", "printtokens2"},
-				{"5542", "replace"},
-				{"2650", "schedule"},
-				{"2710", "schedule2"},
-				{"1608", "tcas"},
-				{"1052", "totinfo"}
+				{"363", "sed", "7"},
+				{"213", "gzip", "5"},
+				{"809", "grep", "5"},
+				{"13585", "space", "38"},
+				{"4130", "printtokens", "7"},
+				{"4115", "printtokens2", "10"},
+				{"5542", "replace", "32"},
+				{"2650", "schedule", "9"},
+				{"2710", "schedule2", "10"},
+				{"1608", "tcas", "41"},
+				{"1052", "totinfo", "23"}
 		};
 		
-		if(args.length != 6 && args.length != 5){
+		if(args.length != 6 && args.length != 3){
 			System.out.println("The characteristics of subjects are as follows:");
 			for(int i = 0; i < argvs.length; i++){
-				System.out.println(String.format("%-20s", argvs[i][1]) + argvs[i][0]);
+				System.out.println(String.format("%-20s", argvs[i][0]) + String.format("%-20s", argvs[i][1]) + argvs[i][2]);
 			}
-			System.err.println("\nUsage: subjectMode(0:Siemens; 1:Sir) rootDir subject consoleDir(excluding /) startVersion endVersion" +
-					"\nor Usage: subjectMode(0:Siemens; 1:Sir) rootDir consoleDir(excluding /) startVersion endVersion");
+			System.err.println("\nUsage: subjectMode(0:Siemens; 1:Sir) rootDir subject consoleDir startVersion endVersion" +
+					"\nor Usage: subjectMode(0:Siemens; 1:Sir) rootDir consoleDir");
 			return;
 		}
 		long time0 = System.currentTimeMillis();
 		if(args.length == 6){
-			Client_Ranking c = new Client_Ranking(new File(args[1]), args[2], new File(args[3] + "_v" + args[6] + "-v" + args[7]), Integer.parseInt(args[6]), Integer.parseInt(args[7]));
+			Client_Ranking c = new Client_Ranking(new File(args[1]), args[2], new File(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
 			if(Integer.parseInt(args[0]) == 0){
 				c.runSiemens();
 			}
@@ -537,10 +696,10 @@ public class Client_Ranking {
 				c.runSir();
 			}
 		}
-		else if(args.length == 5){
+		else if(args.length == 3){
 			assert(Integer.parseInt(args[0]) == 0);
 			for(int i = 4; i < argvs.length; i++){
-				Client_Ranking c = new Client_Ranking(new File(args[1]), argvs[i][1], new File(args[2] + "_v" + args[5] + "-v" + args[6], argvs[i][1]), Integer.parseInt(args[5]), Integer.parseInt(args[6]));
+				Client_Ranking c = new Client_Ranking(new File(args[1]), argvs[i][1], new File(args[2]), 1, Integer.parseInt(argvs[i][2]));
 				c.runSiemens();
 			}
 		}
